798f35819c7dcbd2341548306199a713f52b5228
saul2400 <75963036+saul2400@users.noreply.github.com>
2020-12-14 05:42:34 +0200

    Add files via upload

---
+++ temapeacasa.py
@@ -0,0 +1,35 @@
+# Tema pe acasa 1:
+# Data base:
+user_id = "55"
+user_name = input("Name:")
+user_last_name = input("Last Name:")
+user_email= input("Email:")
+user_online = False
+user_rank = 4,5
+
+# output
+print("....:::: Information ::::....")
+print("User id:      ", user_id)
+print("First Name:   ", user_name)
+print("Last Name:    ", user_last_name)
+print("Email:        ", user_email)
+print("Online Status:", user_online)
+print("Rank:         ", user_rank)
+print("##############################")
+
+# Tema pe acasa 2:
+#Data base:
+students_firstname = "John"
+students_lastname  = "Doe"
+students_Age = input("Age:")
+studets_country = input("Country:")
+students_city = input("City:")
+
+# Output:
+print("...::: Info 2 :::...")
+print("First Name:           ", students_firstname)
+print("Last Name:            ", students_lastname)
+print("Age:                  ", students_Age)
+print("Country of provenance:", studets_country) 
+print("City:                 ", students_city)
+print("########################")
