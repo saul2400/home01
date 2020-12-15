37d5b75567a085f941b2b6bc611040643363805d
saul2400 <75963036+saul2400@users.noreply.github.com>
2020-12-15 17:23:06 +0200

    Add files via upload

---
+++ restaurantapp.py
@@ -0,0 +1,34 @@
+# Prices:
+price_pizza = 70.5
+price_salade = 20
+price_soup = 30
+
+# Nr: 
+nr_pizza = int( input("Nr of pizza:") )
+nr_salade = int( input("Nr of salade:") )
+nr_soup = int( input("Nr of soup:") )
+
+# calcul:
+total_pizza = price_pizza * nr_pizza
+total_salade = price_salade * nr_salade
+total_soup = price_soup * nr_soup
+
+total = total_pizza + total_salade + total_soup
+
+# Output:
+
+print("##### Restaurant Menu #####")
+print("#1. Pizza price:", price_pizza)
+print("#2. Salade price:", price_salade)
+print("#3. Soup price:", price_soup)
+print("###########################")
+print("# How many pizza/s do you order:", nr_pizza )
+print("# How many salade/s do you order:", nr_salade )
+print("# How many soup/s do you order:", nr_soup )
+print("###########################")
+print("# Pizza x", nr_pizza , "=", total_pizza)
+print("# Salade x", nr_salade , "=", total_salade)
+print("# Soup x", nr_soup , "=", total_soup)
+print("###########################")
+print("# Total cost:", total)
+print("###########################")
